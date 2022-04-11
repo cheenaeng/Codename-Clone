bec441e6598cbe7ce7d66e8275cbdc9b8a20f832
cheenaeng <cheena94sing@gmail.com>
2022-04-11 09:19:57 +0800

    index mjs changes heroku

--- models/index.mjs
+++ models/index.mjs
@@ -18,7 +18,10 @@ if (env === 'production') {
   const { DATABASE_URL } = process.env;
   const dbUrl = url.parse(DATABASE_URL);
   const username = dbUrl.auth.substr(0, dbUrl.auth.indexOf(':'));
-  const password = dbUrl.auth.substr(dbUrl.auth.indexOf(':') + 1, dbUrl.auth.length);
+  const password = dbUrl.auth.substr(
+    dbUrl.auth.indexOf(':') + 1,
+    dbUrl.auth.length
+  );
   const dbName = dbUrl.path.slice(1);
   const host = dbUrl.hostname;
   const { port } = dbUrl;
