1f9b8a7dd52a3984651aab9d98027932ec4ffb3c
cheenaeng <cheena94sing@gmail.com>
2022-07-01 12:39:51 +0800

    edited some changes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   development: {
-    username: 'cheenaeng' || 'postgres',
+    username: 'cheenaeng',
     password: null,
     database: 'CODENAMES_development',
     host: '127.0.0.1',
